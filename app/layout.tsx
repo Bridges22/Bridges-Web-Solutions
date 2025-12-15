c0642a0a4bd5a85df4801954f573f0b370d6c1e3
Bridges22 <bridgesmwashighadi2@gmail.com>
2025-12-15 15:15:37 +0300

    Added tiktok pixel code

--- app/layout.tsx
+++ app/layout.tsx
@@ -128,10 +128,21 @@ export default function RootLayout({
         <meta name="ICBM" content="-1.286389, 36.817223" />
         <meta name="google-adsense-account" content="ca-pub-7752239683582941" />
         
+        {/* TikTok Pixel */}
+        <Script id="tiktok-pixel" strategy="afterInteractive">
+          {`
+            !function (w, d, t) {
+              w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)));}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+              ttq.load('D4VVHHRC77U5H3D99GJG');
+              ttq.page();
+            }(window, document, 'ttq');
+          `}
+        </Script>
+
         {/* Google Tag Manager */}
         <Script
           strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-4C542QY5B8`}
+          src={"https://www.googletagmanager.com/gtag/js?id=G-4C542QY5B8"}
         />
         <Script
           id="gtag-init"
