b7ee2a174a2f576eac78ba4afcbb6b93a01af099
Bridges22 <bridgesmwashighadi2@gmail.com>
2026-04-09 13:12:26 +0300

    tags changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -111,6 +111,17 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning={true}>
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MDFTH2H9');
+          `}
+        </Script>
+
         {/* Google AdSense */}
         <Script
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
@@ -139,27 +150,19 @@ export default function RootLayout({
           `}
         </Script>
 
-        {/* Google Tag Manager */}
-        <Script
-          strategy="afterInteractive"
-          src={"https://www.googletagmanager.com/gtag/js?id=G-4C542QY5B8"}
-        />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-4C542QY5B8', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
+
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} ${pacifico.variable} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MDFTH2H9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         {children}
       </body>
     </html>
